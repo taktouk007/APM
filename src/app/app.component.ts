ea8ae3ce18130aa1568a193acb5de1684bc757b0
BENALI Ibrahim <ibenali@adaming.fr>
2019-05-02 13:57:44 +0200

    Creation du composant product-list.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template : `
+    <div><h1>{{title}}</h1>
+      <app-products></app-products>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'APM';
+  title = 'Formation Nantes';
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductListComponent } from './home/products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
